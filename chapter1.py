b20f5349f53aa74a6d7b6f6d9c96d05ce3ffe2f8
mayura1996 <mmayurapavan@gmail.com>
2021-05-04 22:47:34 +0530

    chapter 1 - read image/video/webcam feed

--- chapter1.py
+++ chapter1.py
@@ -1,7 +1,20 @@
 import cv2
 print("Package Imported")
 
-img = cv2.imread("Resources/lena.png")
+#read image
+# img = cv2.imread("Resources/lena.png")
+#
+# cv2.imshow("Output",img)
+# cv2.waitKey(0)
 
-cv2.imshow("Output",img)
-cv2.waitKey(0)
+#read from webcam
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(3,480)
+cap.set(10,100) #change brightness
+while True:
+    success,img = cap.read()
+    cv2.imshow("Video",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
