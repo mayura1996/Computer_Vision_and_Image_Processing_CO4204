c78db24a1af9c6b0f3856f1f3f674e79cb8a4ad6
mayura1996 <mmayurapavan@gmail.com>
2021-05-05 00:32:17 +0530

    chapter 2 - functions - greyscale,cannyedge,blur

--- chapter2.py
+++ chapter2.py
@@ -3,6 +3,12 @@ import cv2
 img = cv2.imread("Resources/lena.png")
 
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #greyscaling
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
+imageCanny = cv2.Canny(img,150,200)
 
 cv2.imshow("Gray Image",imgGray)
+cv2.imshow("Blur Image",imgBlur)
+cv2.imshow("Canny Image",imageCanny)
+
+
 cv2.waitKey(0)
